2cc4709f35c15fa7e0ce4c59f6d462f8324d2615
torangsitungkir93 <torangsitungkir93@gmail.com>
2021-02-21 18:32:15 +0700

    Hosting ke Heroku Master

--- projekci3kp/application/config/config.php
+++ projekci3kp/application/config/config.php
@@ -23,7 +23,9 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/Sertifikasi/projekci3kp/';
+$config['base_url'] = 'https://pelatihansertifikasi.herokuapp.com/';
+// http://localhost/Sertifikasi/projekci3kp/
+// Ini link untuk local
 
 /*
 |--------------------------------------------------------------------------
@@ -380,7 +382,7 @@ $config['encryption_key'] = '';
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
+$config['sess_save_path'] = sys_get_temp_dir();
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
--- projekci3kp/application/config/database.php
+++ projekci3kp/application/config/database.php
@@ -74,11 +74,11 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
-    'dsn'    => 'pgsql:host=localhost;port=5432;dbname=projekci3kp;user=postgresql;password=root',
-    'hostname' => 'localhost',
-    'username' => 'postgres',
-    'password' => 'root',
-    'database' => 'projekci3kp',
+    'dsn'    => 'pgsql:host=ec2-54-144-251-233.compute-1.amazonaws.com;port=5432;dbname=d5uvi3k6s0jj4m;user=fycjgtrmjvfdva;password=e12ba3f1295ce7d93214af711b20a0d5481af71f89afaad2bf548cbdd700c33a',
+    'hostname' => 'ec2-54-144-251-233.compute-1.amazonaws.com',
+    'username' => 'fycjgtrmjvfdva',
+    'password' => 'e12ba3f1295ce7d93214af711b20a0d5481af71f89afaad2bf548cbdd700c33a',
+    'database' => 'd5uvi3k6s0jj4m',
     'dbdriver' => 'pdo',
     'dbprefix' => '',
     'pconnect' => FALSE,
